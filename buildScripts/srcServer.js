ecf4e88e7b4b518006fbbb67b9ade4bd10ed559c
Samson Tse <sammy_tse@msn.com>
2017-04-06 13:05:46 -0700

    Adding new files prior to webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var path = require('path');
-var open = require('open');
+import express from 'express';
+import path from 'path';
+import open from 'open';
 
 var port = 3000;
 // using express as a package deployer for developement on local network
--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
-var chalk = require('chalk')
+import chalk from 'chalk';
+//var chalk = require('chalk') standard es 5 (ECMAScript which is a standardization of JS)
+//Babel approach --> the reason that babel is added to node in the package.json
 
 console.log(chalk.green("Starting app in dev mode..."));
