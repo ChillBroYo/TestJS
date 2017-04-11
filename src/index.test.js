2d0b776afd8e6924a557a9e40df76c52b989b27d
Samson Tse <sammy_tse@msn.com>
2017-04-10 22:18:57 -0400

    Added new test cases

--- src/index.test.js
+++ src/index.test.js
@@ -11,14 +11,42 @@ describe('Our first test', () => {
 
 
 //Asynchronous calls require the 'done' parameter in the mocha framework, in order to evaluate multiple tests
+//Test if second h1 has the name Users
 describe('index.html', () => {
-  it('Should say hello', (done) => {
+  it('Should say Users', (done) => {
     const index = fs.readFileSync('./src/index.html', 'utf-8');
     jsdom.env(index, function(err, window){
-      const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("This is the heading, or is it, or is it really");
+      const h1 = window.document.getElementsByTagName('h1')[1];
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
   })
+})
+
+//Test if the first h1 has value This is the heading, or is it, or is it really
+describe('index.html', () => {
+  it('Should have 0 rows to start', (done) => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, function(err, window){
+    const h1 = window.document.getElementsByTagName('h1')[0];
+    expect(h1.innerHTML).to.equal('This is the heading, or is it, or is it really');
+    done();
+    window.close;
+    });
+  })
+})
+
+//Test if the first table has the name outlineTable
+describe('index.html', () => {
+  it('Should have 0 rows to start', (done) => {
+    const index = fs.readFileSync('./src/index.html', 'utf-8');
+    jsdom.env(index, function(err, window){
+    const table = window.document.getElementsByTagName('table')[0];
+    expect(table.id).to.equal('outlineTable');
+    done();
+    window.close;
+    });
+  })
+
 })
